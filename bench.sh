02cf377906c455649107921a57af69d30f4a9b6d
Noah Daniels <ndaniels@mac.com>
2023-11-05 13:22:22 -0500

    stderr to file

--- bench.sh
+++ bench.sh
@@ -3,7 +3,7 @@
 for k in 10 100 ; do
 	for ds in deep-image fashion-mnist gist glove-25 glove-50 glove-100 glove-200 mnist sift lastfm nytimes ; do
 		for rival in faiss hnsw mrpt ; do
-			python -m rivals /scratch/data/ann-benchmarks/datasets $ds $rival $k > $ds_$rival_$k ;
+			python -m rivals /scratch/data/ann-benchmarks/datasets $ds $rival $k > $ds_$rival_$k 2>&1;
 		done
 	done
 done
